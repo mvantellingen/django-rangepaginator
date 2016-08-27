75b057fa874d870baea33b0d46990db65f047965
Michael van Tellingen <michael@mvantellingen.nl>
2016-08-27 19:59:09 +0200

    Add prev/next :-)

--- src/django_rangepaginator/templatetags/rangepaginator.py
+++ src/django_rangepaginator/templatetags/rangepaginator.py
@@ -12,6 +12,7 @@ def paginate(page=None, distance=2, request=None):
     pages = calculate_pages(
         page.number, paginator.num_pages, distance=distance)
 
+    prev_page_url = next_page_url = None
     result = []
     if request:
         parts = urlparse(request.get_full_path())
@@ -24,14 +25,39 @@ def paginate(page=None, distance=2, request=None):
                 query = urlencode(params, doseq=True)
                 url = urlunparse(parts[:4] + (query,) + parts[5:])
                 result.append((page_num, url))
+
+        if page.has_previous():
+            params['page'] = page.previous_page_number()
+            query = urlencode(params, doseq=True)
+            prev_page_url = urlunparse(parts[:4] + (query,) + parts[5:])
+
+        if page.has_next():
+            params['page'] = page.next_page_number()
+            query = urlencode(params, doseq=True)
+            next_page_url = urlunparse(parts[:4] + (query,) + parts[5:])
+
     else:
         for page_num in pages:
             url = '?%s' % urlencode({'page': str(page_num)})
             result.append((page_num, url))
+
+        if page.has_previous():
+            prev_page_url = '?%s' % urlencode({
+                'page': str(page.previous_page_number())
+            })
+
+        if page.has_next():
+            next_page_url = '?%s' % urlencode({
+                'page': str(page.next_page_number())
+            })
+
     pages = result
 
     return {
         'current': page.number,
+        'page': page,
         'pages': pages,
         'paginator': paginator,
+        'next_page_url': next_page_url,
+        'prev_page_url': prev_page_url,
     }
